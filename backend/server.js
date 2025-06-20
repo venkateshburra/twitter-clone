18f529aa1b467463fc6419fb824a3c0c992ffbaf
Venkatesh Burra <burravenkatesh284@gmail.com>
2025-06-20 19:34:50 +0530

    created post with text and image

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 const port = process.env.PORT || 5000
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb"}));
 
 // app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -2,6 +2,9 @@ import { CiImageOn } from "react-icons/ci";
 import { BsEmojiSmileFill } from "react-icons/bs";
 import { useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getAuth } from "../../components/GetAuth";
+import toast from "react-hot-toast";
 
 const CreatePost = () => {
   const [text, setText] = useState("");
@@ -9,16 +12,46 @@ const CreatePost = () => {
 
   const imgRef = useRef(null);
 
-  const isPending = false;
-  const isError = false;
+  const { data: authUser } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: getAuth,
+  });
 
-  const data = {
-    profileImg: "/avatars/boy1.png",
-  };
+  const queryClient = useQueryClient();
+  
+  const { mutate: createPost, isPending, isError, error } = useMutation({
+    mutationFn: async ({text, img}) => {
+      try {
+        const res = await fetch("/api/posts/create", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify({text, img}),
+        });
+
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");   
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+        
+      }
+    },
+    onSuccess: () => {
+      setText("");
+      setImg(null);
+      toast.success("Post created successfully")
+      queryClient.invalidateQueries({ queryKey: ['post']})
+    }
+  })
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert("Post created successfully");
+    createPost({text, img});
   };
 
   const handleImgChange = (e) => {
@@ -27,6 +60,7 @@ const CreatePost = () => {
       const reader = new FileReader();
       reader.onload = () => {
         setImg(reader.result);
+        console.log(reader.result);
       };
       reader.readAsDataURL(file);
     }
@@ -36,7 +70,7 @@ const CreatePost = () => {
     <div className="flex items-start gap-4 p-4 border-b border-gray-700">
       <div className="avatar">
         <div className="w-8 rounded-full">
-          <img src={data.profileImg || "/avatar-placeholder.png"} />
+          <img src={authUser.img || "/avatar-placeholder.png"} />
         </div>
       </div>
       <form className="flex flex-col w-full gap-2" onSubmit={handleSubmit}>
@@ -81,7 +115,7 @@ const CreatePost = () => {
             {isPending ? "Posting..." : "Post"}
           </button>
         </div>
-        {isError && <div className="text-red-500">Something went wrong</div>}
+        {isError && <div className="text-red-500">{error.message || "Something went wrong"}</div>}
       </form>
     </div>
   );
