af8c8ee67a5b1e81a596c8c335449497191e407f
Venkatesh Burra <burravenkatesh284@gmail.com>
2025-06-22 19:17:28 +0530

    notification added

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,83 +1,118 @@
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
+import toast from 'react-hot-toast';
 
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
-		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
-		},
-	];
+	const queryClinet = useQueryClient();
+  const { data: notifications, isLoading } = useQuery({
+    queryKey: ["notifications"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/notifications");
+        const data = await res.json();
 
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
 
-	return (
-		<>
-			<div className='flex-[4_4_0] border-l border-r border-gray-700 min-h-screen'>
-				<div className='flex items-center justify-between p-4 border-b border-gray-700'>
-					<p className='font-bold'>Notifications</p>
-					<div className='dropdown '>
-						<div tabIndex={0} role='button' className='m-1'>
-							<IoSettingsOutline className='w-4' />
-						</div>
-						<ul
-							tabIndex={0}
-							className='dropdown-content z-[1] menu p-2 shadow bg-slate-900 rounded-box w-52'
-						>
-							<li>
-								<a onClick={deleteNotifications}>Delete all notifications</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-				{isLoading && (
-					<div className='flex items-center justify-center h-full'>
-						<LoadingSpinner size='lg' />
-					</div>
-				)}
-				{notifications?.length === 0 && <div className='p-4 font-bold text-center'>No notifications 🤔</div>}
-				{notifications?.map((notification) => (
-					<div className='border-b border-gray-700' key={notification._id}>
-						<div className='flex gap-2 p-4'>
-							{notification.type === "follow" && <FaUser className='w-7 h-7 text-primary' />}
-							{notification.type === "like" && <FaHeart className='text-red-500 w-7 h-7' />}
-							<Link to={`/profile/${notification.from.username}`}>
-								<div className='avatar'>
-									<div className='w-8 rounded-full'>
-										<img src={notification.from.profileImg || "/avatar-placeholder.png"} />
-									</div>
-								</div>
-								<div className='flex gap-1'>
-									<span className='font-bold'>@{notification.from.username}</span>{" "}
-									{notification.type === "follow" ? "followed you" : "liked your post"}
-								</div>
-							</Link>
-						</div>
-					</div>
-				))}
-			</div>
-		</>
-	);
+  const { mutate: deleteNotifications } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/notifications", {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+		return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+
+	onSuccess: () => {
+		toast.success("Notifications deleted succesfully")
+		queryClinet.invalidateQueries({ queryKey: ["notifications"]})
+	},
+	onError: (error) => {
+		toast.error(error.message)
+	}
+  });
+
+
+  return (
+    <>
+      <div className="flex-[4_4_0] border-l border-r border-gray-700 min-h-screen">
+        <div className="flex items-center justify-between p-4 border-b border-gray-700">
+          <p className="font-bold">Notifications</p>
+          <div className="dropdown ">
+            <div tabIndex={0} role="button" className="m-1">
+              <IoSettingsOutline className="w-4" />
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-slate-900 rounded-box w-52"
+            >
+              <li>
+                <a onClick={deleteNotifications}>Delete all notifications</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {isLoading && (
+          <div className="flex items-center justify-center h-full">
+            <LoadingSpinner size="lg" />
+          </div>
+        )}
+        {notifications?.length === 0 && (
+          <div className="p-4 font-bold text-center">No notifications 🤔</div>
+        )}
+        {notifications?.map((notification) => (
+          <div className="border-b border-gray-700" key={notification._id}>
+            <div className="flex gap-2 p-4">
+              {notification.type === "follow" && (
+                <FaUser className="w-7 h-7 text-primary" />
+              )}
+              {notification.type === "like" && (
+                <FaHeart className="text-red-500 w-7 h-7" />
+              )}
+              <Link to={`/profile/${notification.from.username}`}>
+                <div className="avatar">
+                  <div className="w-8 rounded-full">
+                    <img
+                      src={
+                        notification.from.profileImg ||
+                        "/avatar-placeholder.png"
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="flex gap-1">
+                  <span className="font-bold">
+                    @{notification.from.username}
+                  </span>{" "}
+                  {notification.type === "follow"
+                    ? "followed you"
+                    : "liked your post"}
+                </div>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 export default NotificationPage;
